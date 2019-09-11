4f8342f23c42a2b4e58bd4ac5a3cb6e21b76f58e
Marcelo Aguero <marceloaguero@gmail.com>
2019-09-11 16:37:58 -0300

    Add service B
    # Limit the subject line to 50 characters
    # Capitalize the subject line
    # Do not end the subject line with a period
    
    # Separate the subject line from body with a blank line
    # Wrap the body at 72 characters
    # Use the body to explain what and why, not how
    # Why is it necessary? (Bug fix, feature, improvements?)
    # How does the change address the issue? 
    # What side effects does this change have?
    # Include a link to the ticket, if any

---
+++ service-b/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"net"
+	"os"
+	"time"
+
+	runtime "github.com/banzaicloud/logrus-runtime-formatter"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	pb "github.com/marceloaguero/microservices-istio/pb"
+)
+
+type greetingServiceServer struct {
+}
+
+var (
+	greetings []*pb.Greeting
+)
+
+func (s *greetingServiceServer) Greeting(ctx context.Context, req *pb.GreetingRequest) (*pb.GreetingResponse, error) {
+	greetings = nil
+
+	tmpGreeting := pb.Greeting{
+		Id:      uuid.New().String(),
+		Service: "Service-B",
+		Message: "Konnichiwa, from Service-B!",
+		Created: time.Now().Local().String(),
+	}
+
+	greetings = append(greetings, &tmpGreeting)
+
+	return &pb.GreetingResponse{
+		Greeting: greetings,
+	}, nil
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func init() {
+	formatter := runtime.Formatter{ChildFormatter: &log.JSONFormatter{}}
+	formatter.Line = true
+	log.SetFormatter(&formatter)
+	log.SetOutput(os.Stdout)
+	level, err := log.ParseLevel(getEnv("LOG_LEVEL", "info"))
+	if err != nil {
+		log.Error(err)
+	}
+	log.SetLevel(level)
+}
+
+func main() {
+	lis, err := net.Listen("tcp", getEnv("PORT", ":50051"))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreetingServiceServer(s, &greetingServiceServer{})
+	log.Fatal(s.Serve(lis))
+}
